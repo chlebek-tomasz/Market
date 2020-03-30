9bc562a6d723063145089acac3893e1ccb4efd04
chlebek-tomasz <tom123ek@gmail.com>
2020-03-30 12:06:44 +0200

    Initial commit

--- src/main/java/db/migration/V1__Insert_categories.sql
+++ src/main/java/db/migration/V1__Insert_categories.sql
@@ -1,4 +1,4 @@
 INSERT INTO categories(id, name) VALUES (1, 'Motoryzacja');
 INSERT INTO categories(id, name) VALUES (2, 'Elektronika');
-INSERT INTO categories(id, name) VALUES (1, 'Moda');
-INSERT INTO categories(id, name) VALUES (1, 'Dom i ogród');
+INSERT INTO categories(id, name) VALUES (3, 'Moda');
+INSERT INTO categories(id, name) VALUES (4, 'Dom i ogród');
