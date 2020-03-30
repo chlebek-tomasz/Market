aed505e0dcac3d92aa7bde17f88069e72aad46c7
chlebek-tomasz <tom123ek@gmail.com>
2020-03-30 13:08:25 +0200

    Initial commit

--- src/main/resources/db/migration/V1__Insert_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO categories(id, name) VALUES (1, 'Motoryzacja');
-INSERT INTO categories(id, name) VALUES (2, 'Elektronika');
-INSERT INTO categories(id, name) VALUES (3, 'Moda');
-INSERT INTO categories(id, name) VALUES (4, 'Dom i ogród');
-INSERT INTO roles(id, name) VALUES (1, 'USER');
